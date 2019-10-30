1d3ac1219bc4da4673ae5448aa39f30ceb5514e5
Nkeze Sylvester <snkeze9@gmail.com>
2019-10-30 19:39:07 +0100

    Added: product detail and cart item page

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,32 +6,40 @@ $(document).ready(function () {
     var featuredNavRight = $('#featuredNavRight')
     var salesNavLeft = $('#salesNavLeft')
     var salesNavRight = $('#salesNavRight')
+    var poloSlide = $('#polo-carousel')
+    var poloNavLeft = $('#poloNavLeft')
+    var poloNavRight = $('#poloNavRight')
+    var tShirtSlide = $('#tShirt-carousel')
+    var tShirtNavLeft = $('#tShirtNavLeft')
+    var tShirtNavRight = $('#tShirtNavRight')
 
     //General setting for slide on home page
     var generalSetting = {
         stagePadding: 0,
-        responsive:{
-            0:{
-                items:2,
-                nav:false
+        responsive: {
+            0: {
+                items: 2,
+                nav: false
             },
-            768:{
-                items:4,
-                nav:false
+            768: {
+                items: 4,
+                nav: false
             },
-            992:{
-                items:6,
-                nav:false
+            992: {
+                items: 6,
+                nav: false
             }
         }
     }
     featuredSlide.owlCarousel(generalSetting);
     salesSlide.owlCarousel(generalSetting);
+    poloSlide.owlCarousel(generalSetting)
+    tShirtSlide.owlCarousel(generalSetting)
 
     /**
      * @description Trigger event for pushing to previous slide in featured carousel
      */
-    featuredNavLeft.on('click',function(e) {
+    featuredNavLeft.on('click', function (e) {
         e.preventDefault()
         featuredSlide.trigger('prev.owl.carousel')
     })
@@ -39,7 +47,7 @@ $(document).ready(function () {
     /**
      * @description Trigger event for pushing to next slide in featured carousel
      */
-    featuredNavRight.on('click',function(e) {
+    featuredNavRight.on('click', function (e) {
         e.preventDefault()
         featuredSlide.trigger('next.owl.carousel')
     })
@@ -47,7 +55,7 @@ $(document).ready(function () {
     /**
      * @description Trigger event for pushing to previous slide in sales carousel
      */
-    salesNavLeft.on('click',function(e) {
+    salesNavLeft.on('click', function (e) {
         e.preventDefault()
         salesSlide.trigger('prev.owl.carousel')
     })
@@ -55,43 +63,74 @@ $(document).ready(function () {
     /**
      * @description Trigger event for pushing to next slide in sales carousel
      */
-    salesNavRight.on('click',function(e) {
+    salesNavRight.on('click', function (e) {
         e.preventDefault()
         salesSlide.trigger('next.owl.carousel')
     })
-});
+        /**
+     * @description Trigger event for pushing to previous slide in polo carousel
+     */
+    poloNavLeft.on('click', function (e) {
+        e.preventDefault()
+        poloSlide.trigger('prev.owl.carousel')
+    })
+
+    /**
+     * @description Trigger event for pushing to next slide in polo carousel
+     */
+    poloNavRight.on('click', function (e) {
+        e.preventDefault()
+        poloSlide.trigger('next.owl.carousel')
+    })
+
+    /**
+     * @description Trigger event for pushing to previous slide in tshirt carousel
+     */
+    tShirtNavLeft.on('click', function (e) {
+        e.preventDefault()
+        tShirtSlide.trigger('prev.owl.carousel')
+    })
+
+    /**
+     * @description Trigger event for pushing to next slide in tshirt carousel
+     */
+    tShirtNavRight.on('click', function (e) {
+        e.preventDefault()
+        tShirtSlide.trigger('next.owl.carousel')
+    })
 
 
-/*
-**Switch between two modals
-*/
+    /*
+     **Switch between two modals
+     */
 
-function SwitchModals(e){
-    e.preventDefault();
-    var currentModal = e.target.dataset.from;
-    var targetModal = e.target.dataset.to;
-    $(currentModal).modal('hide')
-    $(targetModal).modal('show')
-}
-
-$('.loginLink').click(SwitchModals);
-$('.regLink').click(SwitchModals);
-
-/*
-**Toggle Navlink categories
-*/
-$('#categoryToggle').click(function(e){
-    e.preventDefault();
-    var category = e.target.dataset.tag;
-    $(category).toggleClass('d-none');
-    e.target.classList.toggle('is-open');
-})
-
-$(document).on('click',function(e){
-    $closest = $(e.target)
-    //Close category links if it is not the targeted element
-    if(!$closest.closest('#categoryNavLink').length && !$(e.target).is('#categoryToggle')) {
-        $('#categoryNavLink').addClass('d-none');
-        $('#categoryToggle').removeClass('is-open');
+    function SwitchModals(e) {
+        e.preventDefault();
+        var currentModal = e.target.dataset.from;
+        var targetModal = e.target.dataset.to;
+        $(currentModal).modal('hide')
+        $(targetModal).modal('show')
     }
-})
+
+    $('.loginLink').click(SwitchModals);
+    $('.regLink').click(SwitchModals);
+
+    /*
+     **Toggle Navlink categories
+     */
+    $('#categoryToggle').click(function (e) {
+        e.preventDefault();
+        var category = e.target.dataset.tag;
+        $(category).toggleClass('d-none');
+        e.target.classList.toggle('is-open');
+    })
+
+    $(document).on('click', function (e) {
+        $closest = $(e.target)
+        //Close category links if it is not the targeted element
+        if (!$closest.closest('#categoryNavLink').length && !$(e.target).is('#categoryToggle')) {
+            $('#categoryNavLink').addClass('d-none');
+            $('#categoryToggle').removeClass('is-open');
+        }
+    })
+});
